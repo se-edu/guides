541abd31d7d7b9d168b780a253c5c8c7eb94012f
Damith C. Rajapakse <damith@gmail.com>
2022-09-04 16:11:00 +0800

    Site Update. [skip ci]

--- tutorials/textUiTesting.page-vue-render.js
+++ tutorials/textUiTesting.page-vue-render.js
@@ -62,6 +62,6 @@ with(this){return _c('p',[_c('strong',[_v("Authors:")])])}
 with(this){return _c('ul',[_c('li',[_v("Initial Version: based on se-edu/addressbook-level2, adapted by Jeffry Lum")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("[Powered by "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.0.1")]),_v(" on Fri, 2 Sept 2022, 0:09:11 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("[Powered by "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.0.1")]),_v(" on Sun, 4 Sept 2022, 16:10:29 GMT+8]")])])])}
 }];
   
