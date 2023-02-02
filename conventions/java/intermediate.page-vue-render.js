e4385f445aad7bd627e24a13cc120dd87ba3868f
Damith C. Rajapakse <damith@gmail.com>
2023-02-02 18:55:30 +0800

    Site Update. [skip ci]

--- conventions/java/intermediate.page-vue-render.js
+++ conventions/java/intermediate.page-vue-render.js
@@ -308,6 +308,6 @@ with(this){return _c('h2',{attrs:{"id":"contributors"}},[_c('span',{staticClass:
 with(this){return _c('ul',[_c('li',[_v("Nimantha Baranasuriya - Initial draft")]),_v(" "),_c('li',[_v("Dai Thanh - Further tweaks")]),_v(" "),_c('li',[_v("Tong Chun Kit - Further tweaks")]),_v(" "),_c('li',[_v("Barnabas Tan - Converted from Google Docs to Markdown Document")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("[Powered by "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.0.2")]),_v(" on Sat, 28 Jan 2023, 17:14:14 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("[Powered by "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.0.2")]),_v(" on Thu, 2 Feb 2023, 18:55:17 GMT+8]")])])])}
 }];
   
