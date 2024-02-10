14313e54c525128961ecf323df70096d8f580795
damithc <damithc@github>
2024-02-10 17:54:15 +0800

    Site Update. [skip ci]

--- conventions/github.page-vue-render.js
+++ conventions/github.page-vue-render.js
@@ -71,6 +71,6 @@ with(this){return _c('h2',{attrs:{"id":"issues"}},[_v("Issues"),_c('a',{staticCl
 with(this){return _c('li',[_c('p',[_c('strong',[_v("Issue title should be concise yet descriptive.")]),_v(" For example, instead of "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("Newbie question, please help")]),_v(", use "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("How do I set up git to ignore test files?")])])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("[Powered by "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.2.0")]),_v(" on Fri, 9 Feb 2024, 17:07:42 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("[Powered by "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.2.0")]),_v(" on Sat, 10 Feb 2024, 17:53:34 GMT+8]")])])])}
 }];
   
