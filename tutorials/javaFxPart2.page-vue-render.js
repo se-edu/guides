1cfe1517332beee6df399beaf5f3ea3a07ed6ad7
damithc <damithc@github>
2024-09-29 17:30:53 +0800

    Site Update. [skip ci]

--- tutorials/javaFxPart2.page-vue-render.js
+++ tutorials/javaFxPart2.page-vue-render.js
@@ -104,6 +104,6 @@ with(this){return _c('p',[_c('strong',[_v("Authors:")])])}
 with(this){return _c('ul',[_c('li',[_v("Initial Version: Jeffry Lum")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("[Powered by "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(" on Sun, 29 Sept 2024, 17:03:53 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("[Powered by "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(" on Sun, 29 Sept 2024, 17:29:46 GMT+8]")])])])}
 }];
   
